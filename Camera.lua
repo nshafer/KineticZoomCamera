70ec2aa770421bb3724cab5dddd2b146d6c2003d
Nathan Shafer / Hoven <nate@lotech.org>
2013-06-13 10:55:16 -0700

    Added license

--- Camera.lua
+++ Camera.lua
@@ -1,7 +1,31 @@
+-- Kinetic Zoom Camera
+-- https://github.com/nshafer/KineticZoomCamera
+
+-- The MIT License (MIT)
+
+-- Copyright (c) 2013 Nathan Shafer
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+-- THE SOFTWARE.
+
+
 --[[
 
-	Camera class.
-	
 	This implements a camera class that allows the user to drag and zoom a virtual camera.  It works
 	basically by having child elements that are bigger than the view size of the given device.  There
 	isn't really a camera that moves, rather it just moves itself and any child elements relative
